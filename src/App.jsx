fd266c84da3a899bb65f564e2f2bac1210163dab
Barhoud Hafsa <barhoudhafsa@gmail.com>
2026-01-18 18:58:50 +0100

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,20 @@ function App() {
 
 
   return (
-    <>
-      <div className="text-2xl bg-red-900 text-white">hello</div>
-    </>
+    <div className="bg-slate-300 font-outfit text-[15px] w-full h-screen flex justify-center items-center ">
+      <div className="bg-white w-1/5 rounded-lg p-3 flex-col justify-center items-center">
+        <div className="rounded-lg overflow-hidden">
+          <img src="/image-qr-code.png" alt="qr-image" />
+        </div>
+        <h2 className="text-center font-bold text-slate-900 py-2">Improve your front-end skills by building projects</h2>
+        <p className="text-center text-slate-500">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+
+        <div class="attribution" className="pt-1 text-center">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" className="text-slate-600" target="_blank">Frontend Mentor</a>. 
+          Coded by <a href="#" className="text-slate-600">Hapeusa</a>.
+        </div>
+      </div>
+    </div>
   )
 }
 
